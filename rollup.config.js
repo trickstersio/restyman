892d9eefbf8018b4635beca0c43e572655bac8f0
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2017-06-07 13:50:32 +0300

    Init

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import babel from 'rollup-plugin-babel';
+import filesize from 'rollup-plugin-filesize';
+import uglify from 'rollup-plugin-uglify';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const destBase = 'dist/restyman'
+const destExtension = `${isProduction ? '.min' : ''}.js`;
+
+export default {
+  entry: 'src/index.js',
+  moduleName: 'restyman',
+  targets: [
+    { dest: `${destBase}${destExtension}`, format: 'cjs' },
+  ],
+  plugins: [
+    babel({ babelrc: false, presets: [ 'es2015-rollup', 'stage-1' ] }),
+    isProduction && uglify(),
+    filesize(),
+  ].filter((plugin) => !!plugin)
+};
